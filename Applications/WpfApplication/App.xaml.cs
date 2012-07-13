4810cac48ac83ac985599efc3e18d623b9b6004e
James Pennell <pennell.james@gmail.com>
2012-07-13 15:12:16 -0400

    Add initial project structure

---
+++ Applications/WpfApplication/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace BrockSolutions.Ticker.Applications.WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
